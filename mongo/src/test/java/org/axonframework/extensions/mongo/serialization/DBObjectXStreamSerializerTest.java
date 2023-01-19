aff15f9fd8db26d465b3d97085bfd56c7905c6ee
Gerard Klijs <gerard.klijs@axoniq.io>
2023-01-19 17:55:41 +0100

    Fix build for Java 17.

--- mongo/src/test/java/org/axonframework/extensions/mongo/serialization/DBObjectXStreamSerializerTest.java
+++ mongo/src/test/java/org/axonframework/extensions/mongo/serialization/DBObjectXStreamSerializerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,14 @@
 package org.axonframework.extensions.mongo.serialization;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import org.axonframework.serialization.Revision;
 import org.axonframework.serialization.SerializedObject;
+import org.axonframework.serialization.xml.CompactDriver;
 import org.junit.jupiter.api.*;
 
 import java.io.Serializable;
@@ -28,6 +34,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -46,8 +53,10 @@ class DBObjectXStreamSerializerTest {
 
     @BeforeEach
     void setUp() {
+        XStream xStream = new XStream(new CompactDriver());
+        xStream.registerConverter(new SomeEnumEnumSetConverter());
         this.testSubject = DBObjectXStreamSerializer.builder()
-                                                    .xStream(new XStream())
+                                                    .xStream(xStream)
                                                     .build();
     }
 
@@ -223,5 +232,47 @@ class DBObjectXStreamSerializerTest {
     private static class StubDomainEvent implements Serializable {
 
         private static final long serialVersionUID = 3209191845532896831L;
+    }
+
+    public class SomeEnumEnumSetConverter implements Converter {
+
+        public boolean canConvert(Class type) {
+            //actually only if it's a TestEventWithEnumSet set, but fine for the test.
+            return type != null && EnumSet.class.isAssignableFrom(type);
+        }
+
+        public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
+            EnumSet set = (EnumSet) source;
+            writer.setValue(this.joinEnumValues(set));
+        }
+
+        private String joinEnumValues(EnumSet set) {
+            boolean seenFirst = false;
+            StringBuffer result = new StringBuffer();
+
+            Enum value;
+            for (Iterator iterator = set.iterator(); iterator.hasNext(); result.append(value.name())) {
+                value = (Enum) iterator.next();
+                if (seenFirst) {
+                    result.append(',');
+                } else {
+                    seenFirst = true;
+                }
+            }
+
+            return result.toString();
+        }
+
+        public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+            EnumSet set = EnumSet.noneOf(TestEventWithEnumSet.SomeEnum.class);
+            String[] enumValues = reader.getValue().split(",");
+            for (int i = 0; i < enumValues.length; ++i) {
+                String enumValue = enumValues[i];
+                if (enumValue.length() > 0) {
+                    set.add(TestEventWithEnumSet.SomeEnum.valueOf(enumValue));
+                }
+            }
+            return set;
+        }
     }
 }
