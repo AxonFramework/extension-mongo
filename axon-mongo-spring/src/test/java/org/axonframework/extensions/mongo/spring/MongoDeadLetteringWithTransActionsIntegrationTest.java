f18e228fc50db14833dad2845a3b5e9663a0179c
Gerard Klijs <gerard.klijs@axoniq.io>
2023-04-26 15:28:52 +0200

    Update axon-mongo-spring/src/test/java/org/axonframework/extensions/mongo/spring/MongoDeadLetteringWithTransActionsIntegrationTest.java
    Co-authored-by: Steven van Beelen <steven.vanbeelen@axoniq.io>

--- axon-mongo-spring/src/test/java/org/axonframework/extensions/mongo/spring/MongoDeadLetteringWithTransActionsIntegrationTest.java
+++ axon-mongo-spring/src/test/java/org/axonframework/extensions/mongo/spring/MongoDeadLetteringWithTransActionsIntegrationTest.java
@@ -31,7 +31,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public class MongoDeadLetteringWithTransActionsIntegrationTest extends DeadLetteringEventIntegrationTest {
+class MongoDeadLetteringWithTransactionsIntegrationTest extends DeadLetteringEventIntegrationTest {
 
     @Container
     private static final MongoDBContainer MONGO_CONTAINER = new MongoDBContainer("mongo:5");
